feeee2f59c2d1d8e3d112c0b22e90f8c309e7f21
Domore-kr <8749132@mail.ru>
2023-01-15 20:04:45 +0500

    draw

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var phone = document.querySelector(".tel");
+
+phone.addEventListener("input", changeBackground);
+
+function changeBackground() {
+  if (phone.value !== "") {
+    document.querySelector("button").style.background = "orange";
+    document.querySelector("button").style.color = "white";
+  } else {
+    document.querySelector("button").style.background = "green";
+  }
+}
