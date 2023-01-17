a91e8fc839ba5fa2f3ff23a94e34ebbd694cd11e
Domore-kr <8749132@mail.ru>
2023-01-18 01:37:10 +0500

    auth

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ var phone = document.querySelector(".tel");
 phone.addEventListener("input", changeBackground);
 
 function changeBackground() {
-  if (phone.value !== "") {
-    document.querySelector("button").style.background = "orange";
+  if (phone.value !== "" && phone.value.length > 11) {
+    document.querySelector("button").style.background = "#f8bc33";
+    document.querySelector("button").style["boxShadow"] = "5px 10px #e59d21";
     document.querySelector("button").style.color = "white";
+  } else {
+    document.querySelector("button").style.background = "#61c32c";
+    document.querySelector("button").style.color = "#36840c";
   }
 }
